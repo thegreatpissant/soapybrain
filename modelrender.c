21f9e99b5d59049d796c517ae47673b739b9d45d
James A. Feister <oenjaf@gmail.com>
2012-07-11 23:49:06 -0400

    We can now render md5 skeletons

--- modelrender.c
+++ modelrender.c
@@ -6,19 +6,48 @@
 #include <GL/glx.h>
 #include <GL/glut.h>
 #include <GL/freeglut_ext.h>
+#include "charset.h"
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define GWH 800
+
+pskeleton mySkeleton;
+#define ZCORD -50.0
+#define XCORD 0.0
+#define YCORD 0.0
+GLfloat zCord = ZCORD;
+GLfloat xCord = XCORD;
+GLfloat yCord = YCORD;
+GLint xmouse_press = 0;
+GLint ymouse_press = 0;
+GLfloat x_rotate = 0.0, y_rotate = 0.0;
+GLboolean rotate_obj = GL_FALSE;
 
 void glut_display_func (void)
 {
   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity ();
+  glTranslatef (xCord, yCord, zCord);
+ 
+  glPushMatrix (); {
+    glRotatef (y_rotate, 0.0, 1.0, 0.0);
+    glRotatef (x_rotate, 1.0, 0.0, 0.0);
+   
+    glBegin (GL_POINTS);
+    glColor3f( 1.0, 1.0f, 1.0f);
+    int dxi;
+    for (dxi = 0; dxi < mySkeleton->numJoints; dxi++)
+      {
+	glVertex3f (mySkeleton->joints[dxi]->posX,
+		    mySkeleton->joints[dxi]->posY,
+		    mySkeleton->joints[dxi]->posZ);
+      }
 
-
-  glEnd ();
+    glEnd ();
+  }  glPopMatrix ();
   glutSwapBuffers ();
 }
 
@@ -39,12 +68,63 @@ void glut_reshape_func (int w, int h)
   glLoadIdentity ();
 }
 
-void glut_mouse_func (int button, int state, int x, int y)
+void glut_idle_func ()
 {
 }
 
-void glut_idle_func ()
+void glut_mouse_func ( int button, int state, int x, int y)
 {
+  switch (button)
+    {
+    case GLUT_DOWN:
+      xmouse_press = x;
+      ymouse_press = y;
+      rotate_obj = GL_TRUE;
+      break;
+    case GLUT_UP:
+      rotate_obj = GL_FALSE;
+      break;
+    default:
+      break;
+    }
+  glutPostRedisplay ();
+}
+
+void glut_motion_func (int x, int y)
+{
+  x_rotate += (GLfloat)((xmouse_press - x) *0.5);
+  y_rotate += (GLfloat)((ymouse_press - y) *0.5);
+  glutPostRedisplay ();
+}
+
+void glut_keyboard_func (unsigned char key, int x, int y)
+{
+  switch (key)
+    {
+    case 27:
+      exit (0);
+    case 's':
+      zCord -= 1.0f;
+      break;
+    case 'w':
+      zCord += 1.0f;
+      break;
+    case 'a':
+      xCord -= 1.0f;
+      break;
+    case 'd':
+      xCord += 1.0f;
+      break;
+    case 't':
+      yCord += 1.0f;
+      break;
+    case 'g':
+      yCord -= 1.0f;
+      break;
+    default:
+      break;
+    }
+  glutPostRedisplay ();
 }
 
 int main (int argc, char ** argv)
@@ -62,7 +142,7 @@ int main (int argc, char ** argv)
       fprintf (stderr, "Opening file \"%s\", failed\n", argv[1]);
       exit (EXIT_FAILURE);
     }
-  pskeleton mySkeleton;
+
   mySkeleton = md5mesh_loadfile (md5mesh_file);
       
   glutInit (&argc, argv);
@@ -70,13 +150,14 @@ int main (int argc, char ** argv)
   glutInitWindowSize (GWH, GWH);
   glutInitWindowPosition (200, 100);
   
-  int window = glutCreateWindow ("Model Render");
+  glutCreateWindow ("Model Render");
   init_view();
   glutDisplayFunc (glut_display_func);
   glutReshapeFunc (glut_reshape_func);
-  glutMouseFunc   (glut_mouse_func);
   glutIdleFunc    (glut_idle_func);
-
+  glutKeyboardFunc(glut_keyboard_func);
+  glutMouseFunc   (glut_mouse_func);
+  glutMotionFunc  (glut_motion_func);
   glutMainLoop ();
   skeletonCleanUp (mySkeleton);
   return 0;
