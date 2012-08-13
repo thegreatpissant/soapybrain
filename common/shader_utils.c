6cf6b7c31273fca04cc89d1478233f81167d6bb8
James A. Feister <oenjaf@gmail.com>
2012-08-12 23:33:54 -0400

    Fixed a minor mistype in shader_utils.c, Reminder: always check with a clean recompilation

--- common/shader_utils.c
+++ common/shader_utils.c
@@ -73,7 +73,7 @@ GLuint create_shader (const char *filename, GLenum type)
     #ifdef GL_ES_VERSION_2_0
     "#version 100\n"
     #else
-    "#version 120\n",
+    "#version 120\n"
     #endif
     ,
     /* GLES2 precision specifiers */
