3d9594e3de51a229cad9aaf01341786e373fbba9
thegreatpissant <thegreatpissant@gmail.com>
2013-08-21 23:06:56 -0400

    Chaged readme requirements to specify opengl 2.0, need to verify but this is a placeholder to enforce a better description. Initializer cleaned up.

--- opengl-programing-guide/common/Actor.h
+++ opengl-programing-guide/common/Actor.h
@@ -7,13 +7,11 @@
 class Actor : public Entity {
  public:
   ModelID model_id;
-  Actor () : Entity(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
+  Actor () : Entity(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f), model_id {0}
   {
-    model_id = 0;
   };
-  Actor (float px, float py, float pz, float ox, float oy, float oz, ModelID mid =0 ):Entity(px, py, pz, ox, oy, oz)
+  Actor (float px, float py, float pz, float ox, float oy, float oz, ModelID mid =0 ):Entity(px, py, pz, ox, oy, oz), model_id {mid}
     {
-      model_id = mid;
     };
 };
 
--- opengl-programing-guide/common/shader_utils.h
+++ opengl-programing-guide/common/shader_utils.h
@@ -2,13 +2,12 @@
 #define __SHADER_UTILS_H__
 #include <GL/glew.h>
 #include <GL/glut.h>
-void print_log (GLuint object);
-GLuint create_shader (const char *filename, GLenum type);
 
 typedef  struct shaderinfo {
   GLuint shadertype;
   const char * filename;
 } ShaderInfo;
+
 GLuint LoadShaders(ShaderInfo * si);
 
 #endif
